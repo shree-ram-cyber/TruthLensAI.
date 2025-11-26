81c3ff33db64e0a3b4eb8fa222a71da22d843e1b
aadya2015100 <aadya.2015100@gmail.com>
2025-11-26 20:21:46 +0530

    app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ with st.container():
     gender = st.radio("Select your gender:", ["Male", "Female", "Other"])
     
     # Platform input
-    platform = st.selectbox("Select the platform where you found the news:", ["Instagram", "YouTube", "Facebook", "X"])
+    platform = st.selectbox("Select the platform where you found the news:", ["Instagram", "YouTube", "Facebook", "X", "News Channel"])
     
     st.write("---")
     
